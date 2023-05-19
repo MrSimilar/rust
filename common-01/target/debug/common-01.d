21b0c900023326a99e16e11c698f1b37dedbea34
yuannan <xinyet@aliyun.com>
2023-05-20 00:42:41 +0800

    init

---
+++ common-01/target/debug/common-01.d
@@ -0,0 +1,1 @@
+/Users/yuannan/CLionProjects/rust/common-01/target/debug/common-01: /Users/yuannan/CLionProjects/rust/common-01/src/main.rs
---
+++ common-01/target/debug/deps/common_01-39cfabf5985f69ed.d
@@ -0,0 +1,5 @@
+/Users/yuannan/CLionProjects/rust/common-01/target/debug/deps/common_01-39cfabf5985f69ed: src/main.rs
+
+/Users/yuannan/CLionProjects/rust/common-01/target/debug/deps/common_01-39cfabf5985f69ed.d: src/main.rs
+
+src/main.rs:
